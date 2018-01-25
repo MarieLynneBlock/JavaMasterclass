489387b6ab19cabd224bc604400b56bc7922fd01
Marie-Lynne Block <marielynne.block@gmail.com>
2018-01-25 22:46:13 +0100

    Upload Char and Boolean

---
+++ Section 02/006 Char and Boolean/Main.java
@@ -0,0 +1,13 @@
+package com.marielynneblock;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char myChar = '\u00A9';
+        System.out.println("Unicode output was: " + myChar);
+
+
+     boolean myBoolean = false;
+     boolean isMale = true;
+    }
+}
